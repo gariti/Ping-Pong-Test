d103243c334110d5d3db84650b7036e65dea4420
Garrett Carver <gariti@gmail.com>
2015-10-16 15:20:03 -0700

    add for loop with branching conditions (still needs debugging)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,20 @@
 jQuery(document).ready(function() {
-  
   //user click on 'Let's Play!' button
   $("form").submit(function(event) {
-      event.preventDefault(); //prevent default form submit
+    event.preventDefault(); //prevent default form submit
+    var userNumber = $("#input-field").val(); //get value from input-field
+    alert("You entered: " + userNumber + "!");
+    //loop from 1 to the number the user entered
+    for (var i = 1; i >= userNumber; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        $("ul").append("<li>ping-pong</li>"); //if divisible by 3 and 5 insert 'ping-pong'
+      } else if (i % 3 === 0) {
+        $("ul").append("<li>ping</li>"); //if divisible by 3 insert 'ping'
+      } else if (i % 5 === 0) {
+        $("ul").append("<li>pong</li>"); //if divisible by 5 insert 'pong'  
+      } else {
+        $("ul").append("<li>" + i + "</li>"); //else insert current number  
+      }
+    };
   });
-
 });

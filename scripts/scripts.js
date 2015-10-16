6f3e3fe6074310e6ad639ff98f34f40548258559
Garrett Carver <gariti@gmail.com>
2015-10-16 14:54:54 -0700

    fix anchor tag, add prevent default action when submitting form

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
   
   //user click on 'Let's Play!' button
   $("form").submit(function(event) {
-
+      event.preventDefault(); //prevent default form submit
   });
 
 });
